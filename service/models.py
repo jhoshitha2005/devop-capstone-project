3541ecb8fc54b2e7345636b1401b0c8dbfac8073
jhoshitha2005 <jhoshithamyneni1@gmail.com>
2025-05-22 15:20:21 +0530

    Update models.py

--- service/models.py
+++ service/models.py
@@ -14,54 +14,42 @@ db = SQLAlchemy()
 
 
 class DataValidationError(Exception):
-    """Used for an data validation errors when deserializing"""
+    """Used for data validation errors when deserializing"""
 
 
 def init_db(app):
     """Initialize the SQLAlchemy app"""
-    Account.init_db(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
 
 ######################################################################
 #  P E R S I S T E N T   B A S E   M O D E L
 ######################################################################
 class PersistentBase:
-    """Base class added persistent methods"""
+    """Base class adding persistent methods"""
 
     def __init__(self):
         self.id = None  # pylint: disable=invalid-name
 
     def create(self):
-        """
-        Creates a Account to the database
-        """
+        """Creates an Account to the database"""
         logger.info("Creating %s", self.name)
-        self.id = None  # id must be none to generate next primary key
+        self.id = None  # id must be None to generate next primary key
         db.session.add(self)
         db.session.commit()
 
     def update(self):
-        """
-        Updates a Account to the database
-        """
+        """Updates an Account in the database"""
         logger.info("Updating %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """Removes a Account from the data store"""
+        """Removes an Account from the data store"""
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
-
-    @classmethod
-    def init_db(cls, app):
-        """Initializes the database session"""
-        logger.info("Initializing database")
-        cls.app = app
-        # This is where we initialize SQLAlchemy from the Flask app
-        db.init_app(app)
-        app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
 
     @classmethod
     def all(cls):
@@ -71,7 +59,7 @@ class PersistentBase:
 
     @classmethod
     def find(cls, by_id):
-        """Finds a record by it's ID"""
+        """Finds a record by its ID"""
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
@@ -84,33 +72,31 @@ class Account(db.Model, PersistentBase):
     Class that represents an Account
     """
 
-    app = None
-
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    email = db.Column(db.String(64))
-    address = db.Column(db.String(256))
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    address = db.Column(db.String(256), nullable=False)
     phone_number = db.Column(db.String(32), nullable=True)  # phone number is optional
-    date_joined = db.Column(db.Date(), nullable=False, default=date.today())
+    date_joined = db.Column(db.Date, nullable=False, default=date.today)  # pass function, not call it
 
     def __repr__(self):
         return f"<Account {self.name} id=[{self.id}]>"
 
     def serialize(self):
-        """Serializes a Account into a dictionary"""
+        """Serializes an Account into a dictionary"""
         return {
             "id": self.id,
             "name": self.name,
             "email": self.email,
             "address": self.address,
             "phone_number": self.phone_number,
-            "date_joined": self.date_joined.isoformat()
+            "date_joined": self.date_joined.isoformat() if self.date_joined else None
         }
 
     def deserialize(self, data):
         """
-        Deserializes a Account from a dictionary
+        Deserializes an Account from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
@@ -130,13 +116,14 @@ class Account(db.Model, PersistentBase):
         except TypeError as error:
             raise DataValidationError(
                 "Invalid Account: body of request contained "
-                "bad or no data - " + error.args[0]
+                "bad or no data - " + str(error)
             ) from error
         return self
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all Accounts with the given name
+        """
+        Returns all Accounts with the given name
 
         Args:
             name (string): the name of the Accounts you want to match
